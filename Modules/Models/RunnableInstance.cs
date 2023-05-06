8fb1d906c98ec05d68e84725cdc32d767da8c4ef
Drew Chase <dcmanproductions@gmail.com>
2023-05-06 15:30:58 -0400

    added runnable instance model

---
+++ Modules/Models/RunnableInstance.cs
@@ -0,0 +1,23 @@
+﻿// LFInteractive LLC. - All Rights Reserved
+using System.ComponentModel.DataAnnotations;
+
+namespace Chase.WebDeploy.Modules.Models;
+
+public readonly struct RunnableInstance
+{
+    [Required]
+    public string WorkingDirectory { get; }
+
+    [Required]
+    public string StartupExecutable { get; }
+
+    [Required]
+    public int Port { get; }
+
+    public RunnableInstance(string workingDirectory, string startupExecutable, int port)
+    {
+        WorkingDirectory = workingDirectory;
+        StartupExecutable = startupExecutable;
+        Port = port;
+    }
+}
