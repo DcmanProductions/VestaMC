fe6561f7235e80469ab1b60ea7a0f488e1b2d509
Drew Chase <dcmanproductions@gmail.com>
2023-05-06 15:17:00 -0400

    updated properties

--- Core/Models/InstanceModel.cs
+++ Core/Models/InstanceModel.cs
@@ -1,15 +1,29 @@
 ﻿// LFInteractive LLC. - All Rights Reserved
-namespace Core.Models;
+
+using Chase.WebDeploy.Core.Types;
+
+namespace Chase.WebDeploy.Core.Models;
 
 public struct InstanceModel
 {
+    public Guid Id { get; } = Guid.NewGuid();
+
+    public string Name { get; set; } = "";
+
+    public string Description { get; set; } = "";
+
+    public InstanceState State { get; set; }
+
+    public string Type { get; set; } = "";
+
     public InstanceModel()
     {
     }
 
-    public string Description { get; set; } = "";
-    public Guid Id { get; } = Guid.NewGuid();
-    public string Name { get; set; } = "";
-    public InstanceState State { get; set; }
-    public string Type { get; set; } = "";
+    public struct Domain
+    {
+        public string DomainName { get; set; }
+        public int Port { get; set; }
+        public Domain[] SubDomains { get; set; }
+    }
 }
