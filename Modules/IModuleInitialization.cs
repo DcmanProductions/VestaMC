7f8ba3b0be67a8ae21d03362d57fc5e360e239b8
Drew Chase <dcmanproductions@gmail.com>
2023-05-06 15:20:29 -0400

    added ModuleInitialization base class

---
+++ Modules/IModuleInitialization.cs
@@ -0,0 +1,20 @@
+﻿// LFInteractive LLC. - All Rights Reserved
+using Chase.WebDeploy.Modules.Models;
+
+namespace Chase.WebDeploy.Modules;
+
+public interface IModuleInitialization
+{
+    async Task Initialize() { }
+
+    string GetCommand(RunnableInstance runnableInstance);
+
+    /// <summary> This is the uri of the modules needed binaries<br> For example if your module
+    /// requires a specific executable to operate, instead of bundling it with the module, link a
+    /// direct download to the archive </summary> <returns>The direct uri for the archive</returns>
+    Uri? GetContentArchiveSource();
+
+    ServerArchitectureModel GetServerArchitecture();
+
+    string GetModuleIdentifier();
+}
