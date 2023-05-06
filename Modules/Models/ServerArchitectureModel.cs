060c68f402125f47f58ce9dcf0ac992a300ece53
Drew Chase <dcmanproductions@gmail.com>
2023-05-06 15:30:48 -0400

    added server architecture model

---
+++ Modules/Models/ServerArchitectureModel.cs
@@ -0,0 +1,11 @@
+﻿// LFInteractive LLC. - All Rights Reserved
+
+namespace Chase.WebDeploy.Modules.Models;
+
+public struct ServerArchitectureModel
+{
+    public Guid Id { get; set; }
+    public string Name { get; set; }
+    public string Description { get; set; }
+    public string Executeable { get; set; }
+}
