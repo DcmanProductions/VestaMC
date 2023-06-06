035049f3494da556f64f68af6ba7b8bc62f4096d
Drew Chase <dcmanproductions@gmail.com>
2023-06-06 04:47:09 -0400

    added version list model

---
+++ Minecraft/Models/MinecraftVersionListModel.cs
@@ -0,0 +1,15 @@
+﻿/*
+VestaMC - LFInteractive LLC. (c) 2020-2024
+a minecraft server hosting platform for windows and linux
+https://github.com/dcmanproductions/VestaMC
+Licensed under the GNU General Public License v3.0
+https://www.gnu.org/licenses/lgpl-3.0.html
+*/
+
+namespace Chase.VestaMC.Minecraft.Models;
+
+public struct MinecraftVersionListModel
+{
+    public MinecraftLatestVersionsModel Latest { get; set; }
+    public MinecraftVersionModel[] Versions { get; set; }
+}
