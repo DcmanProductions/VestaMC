e86e72dab7062154b9cc9823f87e9f3ee167a79c
Drew Chase <dcmanproductions@gmail.com>
2023-06-06 04:49:39 -0400

    added input javascript for dropdown items

---
+++ Server/wwwroot/js/inputs.js
@@ -0,0 +1,14 @@
+﻿$(".dropdown-item").on('click', e => {
+    let target = $(e.currentTarget);
+    target.addClass('selected');
+    let dropdown = target.parent().parent()
+    dropdown.find('.value').html(target.html())
+    dropdown.attr('value', target.attr('value'))
+    dropdown.blur()
+})
+$(".dropdown").each((_, item) => {
+    item = $(item)
+    let selected = item.find(`.dropdown-item[value='${item.attr('value')}']`)
+    selected.addClass('selected');
+    item.find(`.value`).html(selected.html());
+})
