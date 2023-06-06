8794ec6842552b19bc7b45e8397ac6c3607e8b8f
Drew Chase <dcmanproductions@gmail.com>
2023-06-06 19:05:21 -0400

    added javascript class to handle create minecraft instance

---
+++ Server/wwwroot/js/create-instance.js
@@ -0,0 +1,18 @@
+﻿let modloaderVersionCombo = $("#modloader-version-dropdown, label[for='modloader-version-dropdown']")
+let modloaderVersionLabel = $("label[for='modloader-version-dropdown']")
+modloaderVersionCombo.css('display', 'none')
+let modloaderVersionDropdown = $("#modloader-version-dropdown")
+let modloaderDropdown = $("#modloader-dropdown")
+modloaderDropdown.find(".dropdown-item[value='fabric']").on('click', async () => await loadFabric())
+modloaderDropdown.find(".dropdown-item[value='forge']").on('click', async () => await loadForge())
+modloaderDropdown.find(".dropdown-item[value='vanilla']").on('click', () => modloaderVersionCombo.css('display', 'none'))
+
+
+async function loadFabric() {
+    modloaderVersionCombo.css('display', "")
+    modloaderVersionLabel.html('Fabric Loader');
+}
+async function loadForge() {
+    modloaderVersionCombo.css('display', "")
+    modloaderVersionLabel.html('Forge Loader');
+}
