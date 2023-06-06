219a14de54d74a3012dfe940ad129113d301fb18
Drew Chase <dcmanproductions@gmail.com>
2023-06-06 04:47:37 -0400

    added minecraft api controller

---
+++ Server/Controllers/api/MinecraftAPIController.cs
@@ -0,0 +1,42 @@
+﻿/*
+VestaMC - LFInteractive LLC. (c) 2020-2024
+a minecraft server hosting platform for windows and linux
+https://github.com/dcmanproductions/VestaMC
+Licensed under the GNU General Public License v3.0
+https://www.gnu.org/licenses/lgpl-3.0.html
+*/
+
+using Chase.VestaMC.Minecraft.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chase.VestaMC.Server.Controllers.api;
+
+[ApiController]
+[Produces("application/json")]
+[Route("/api/minecraft")]
+public class MinecraftAPIController : ControllerBase
+{
+    [HttpGet("versions")]
+    public async Task<IActionResult> GetMinecraftVersions()
+    {
+        return Ok(await MinecraftVersionController.GetMinecraftVersions());
+    }
+
+    [HttpGet("versions/release")]
+    public async Task<IActionResult> GetMinecraftReleaseVersions()
+    {
+        return Ok(await MinecraftVersionController.GetMinecraftReleaseVersions());
+    }
+
+    [HttpGet("versions/snapshot")]
+    public async Task<IActionResult> GetMinecraftSnapshotVersions()
+    {
+        return Ok(await MinecraftVersionController.GetMinecraftSnapshotVersions());
+    }
+
+    [HttpGet("version/{ID}")]
+    public async Task<IActionResult> GetMinecraftVersionByID(string id)
+    {
+        return Ok(await MinecraftVersionController.GetMinecraftVersionByID(id));
+    }
+}
