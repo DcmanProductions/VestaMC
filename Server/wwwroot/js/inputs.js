a7941738fbd558d925c3648b2cf7dd6f95706d0e
Drew Chase <dcmanproductions@gmail.com>
2023-06-07 00:01:15 -0400

    added function to register dropdown item click event

--- Server/wwwroot/js/inputs.js
+++ Server/wwwroot/js/inputs.js
@@ -1,20 +1,22 @@
-﻿$(".dropdown-item").on('click', e => {
-    let target = $(e.currentTarget);
-    let dropdown = target.parent().parent()
-
-    dropdown.each((_, d) => {
-        d = $(d)
-        d.find('.dropdown-item.selected').removeClass('selected')
-    })
-
-    dropdown.find('.value').html(target.html())
-    dropdown.attr('value', target.attr('value'))
-    dropdown.blur()
-    target.addClass('selected');
-})
+﻿$(".dropdown-item").on('click', e => RegisterDropdownItemClickEvent(e.currentTarget))
 $(".dropdown").each((_, item) => {
     item = $(item)
     let selected = item.find(`.dropdown-item[value='${item.attr('value')}']`)
     selected.addClass('selected');
     item.find(`.value`).html(selected.html());
 })
+
+function RegisterDropdownItemClickEvent(dropdownItem) {
+    dropdownItem = $(dropdownItem)
+    let dropdown = dropdownItem.parent().parent()
+
+    dropdown.each((_, d) => {
+        d = $(d)
+        d.find('.dropdown-item.selected').removeClass('selected')
+    })
+
+    dropdown.find('.value').html(dropdownItem.html())
+    dropdown.attr('value', dropdownItem.attr('value'))
+    dropdown.blur()
+    dropdownItem.addClass('selected');
+}
