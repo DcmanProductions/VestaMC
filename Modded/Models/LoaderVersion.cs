2333f550afcfbcfe4beedabebdca14c04dec13ba
Drew Chase <dcmanproductions@gmail.com>
2023-06-07 00:46:43 -0400

    removed static fabric download uri

--- Modded/Models/LoaderVersion.cs
+++ Modded/Models/LoaderVersion.cs
@@ -6,28 +6,21 @@ Licensed under the GNU General Public License v3.0
 https://www.gnu.org/licenses/lgpl-3.0.html
 */
 
-using Chase.VestaMC.Modded.Controllers;
-using Newtonsoft.Json;
-
 namespace Chase.VestaMC.Modded.Models;
 
 public struct LoaderVersion
 {
-    [JsonIgnore]
     public int Major { get; set; }
 
-    [JsonIgnore]
     public int Minor { get; set; }
 
-    [JsonIgnore]
     public int Patch { get; set; }
 
-    [JsonIgnore]
     public string Build { get; set; }
 
-    [JsonProperty]
     public string Version => $"{Major}.{Minor}.{Patch}{Build}";
 
-    [JsonProperty]
-    public Uri DownloadUri => new($"{FabricVersionController.FabricMavenUrl}{Version}/fabric-loader-{Version}.jar");
+    public string? MinecraftVersion { get; set; }
+
+    public Uri DownloadUri { get; set; }
 }
