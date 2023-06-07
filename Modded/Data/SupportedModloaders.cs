9a37244b06db2d5b4028370219d7f09c2996c51d
Drew Chase <dcmanproductions@gmail.com>
2023-06-06 23:45:32 -0400

    added enum for supported modloaders

---
+++ Modded/Data/SupportedModloaders.cs
@@ -0,0 +1,16 @@
+﻿/*
+VestaMC - LFInteractive LLC. (c) 2020-2024
+a minecraft server hosting platform for windows and linux
+https://github.com/dcmanproductions/VestaMC
+Licensed under the GNU General Public License v3.0
+https://www.gnu.org/licenses/lgpl-3.0.html
+*/
+
+namespace Chase.VestaMC.Modded.Data;
+
+public enum SupportedModloaders
+{
+    Vanilla,
+    Forge,
+    Fabric
+}
