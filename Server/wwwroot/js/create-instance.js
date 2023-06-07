912bd5329f7bb85a5c347b57388f54a2cb8c3f60
Drew Chase <dcmanproductions@gmail.com>
2023-06-07 00:01:35 -0400

    now using RegisterDropDownItemClickEvent to handle dropdown item click event

--- Server/wwwroot/js/create-instance.js
+++ Server/wwwroot/js/create-instance.js
@@ -1,42 +1,32 @@
-﻿let modloaderVersionCombo = $("#modloader-version-dropdown, label[for='modloader-version-dropdown']")
-let modloaderVersionLabel = $("label[for='modloader-version-dropdown']")
-modloaderVersionCombo.css('display', 'none')
-let modloaderVersionDropdown = $("#modloader-version-dropdown")
-let modloaderDropdown = $("#modloader-dropdown")
-modloaderDropdown.find(".dropdown-item[value='fabric']").on('click', async () => await loadFabric())
-modloaderDropdown.find(".dropdown-item[value='forge']").on('click', async () => await loadForge())
-modloaderDropdown.find(".dropdown-item[value='vanilla']").on('click', () => modloaderVersionCombo.css('display', 'none'))
+﻿let modloaderVersionCombo = $("#modloader-version-dropdown, label[for='modloader-version-dropdown']");
+let modloaderVersionLabel = $("label[for='modloader-version-dropdown']");
+modloaderVersionCombo.css('display', 'none');
+let modloaderVersionDropdown = $("#modloader-version-dropdown");
+let modloaderDropdown = $("#modloader-dropdown");
+modloaderDropdown.find(".dropdown-item").on('click', async e => await loadModded($(e.currentTarget).attr('value')));
+modloaderDropdown.find(".dropdown-item[value='vanilla']").on('click', () => modloaderVersionCombo.css('display', 'none'));
 
-async function loadFabric() {
+async function loadModded(loader) {
+    // If the loader is vanilla ignore it.
+    if (loader == "vanilla") return;
+
+    // Set the dropdown and label to be visible
     modloaderVersionCombo.css('display', "")
-    modloaderVersionLabel.html('Fabric Loader');
-    let response = await $.get("/api/fabric/versions")
+    modloaderVersionLabel.html(`${loader} loader`); // Set the label text
+
+    // Make the call to the api
+    let response = await $.get(`/api/${loader}/versions`)
+
+    // Loop through all json items and create html dropdown item.
     let items = "";
     Array.from(response).forEach(item => {
         items += `<div class="dropdown-item" value="${item.version}">${item.version}</div>`
     })
+
+    // Insert html into the dropdown items
     modloaderVersionDropdown.find('.dropdown-items').html(items)
-    modloaderVersionDropdown.find('.dropdown-item').each((_, item) => {
-        target = $(item);
-        target.on('click', e => {
-            let target = $(e.currentTarget);
-            let dropdown = target.parent().parent()
 
-            dropdown.each((_, d) => {
-                d = $(d)
-                d.find('.dropdown-item.selected').removeClass('selected')
-            })
-
-            dropdown.find('.value').html(target.html())
-            dropdown.attr('value', target.attr('value'))
-            dropdown.blur()
-            target.addClass('selected');
-        })
-    })
-    modloaderVersionDropdown.find('.dropdown-items')[0].click();
-}
-async function loadForge() {
-    modloaderVersionDropdown.find('.dropdown-items').html("")
-    modloaderVersionCombo.css('display', "")
-    modloaderVersionLabel.html('Forge Loader');
+    // Regsister the click event for the dropdown items
+    modloaderVersionDropdown.find('.dropdown-item').on('click', e => RegisterDropdownItemClickEvent(e.currentTarget))
+    modloaderVersionDropdown.find('.dropdown-item')[0].click()
 }
