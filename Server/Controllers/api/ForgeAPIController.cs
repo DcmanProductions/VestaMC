1c7c0721fa999898a07f710d1dd6641bb40f204e
Drew Chase <dcmanproductions@gmail.com>
2023-06-07 00:47:24 -0400

    added ForgeAPIController

---
+++ Server/Controllers/api/ForgeAPIController.cs
@@ -0,0 +1,30 @@
+﻿/*
+VestaMC - LFInteractive LLC. (c) 2020-2024
+a minecraft server hosting platform for windows and linux
+https://github.com/dcmanproductions/VestaMC
+Licensed under the GNU General Public License v3.0
+https://www.gnu.org/licenses/lgpl-3.0.html
+*/
+
+using Chase.VestaMC.Modded.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chase.VestaMC.Server.Controllers.api;
+
+[ApiController]
+[Produces("application/json")]
+[Route("/api/forge")]
+public class ForgeAPIController : ControllerBase
+{
+    [HttpGet("versions")]
+    public async Task<IActionResult> GetForgeVersions([FromQuery] string mc)
+    {
+        return Ok(await ForgeVersionController.GetForgeLoaderVersions(mc));
+    }
+
+    [HttpGet("version/{ID}")]
+    public async Task<IActionResult> GetForgeVersionByID([FromRoute] string id, [FromQuery] string mc)
+    {
+        return Ok(await ForgeVersionController.GetForgeLoaderVersionByID(mc, id));
+    }
+}
