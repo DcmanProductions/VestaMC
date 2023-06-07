2719815e56df2d0e6f66a116245a7994946ec674
Drew Chase <dcmanproductions@gmail.com>
2023-06-06 23:48:01 -0400

    added call for fabric versions

--- Server/wwwroot/js/create-instance.js
+++ Server/wwwroot/js/create-instance.js
@@ -7,12 +7,36 @@ modloaderDropdown.find(".dropdown-item[value='fabric']").on('click', async () =>
 modloaderDropdown.find(".dropdown-item[value='forge']").on('click', async () => await loadForge())
 modloaderDropdown.find(".dropdown-item[value='vanilla']").on('click', () => modloaderVersionCombo.css('display', 'none'))
 
-
 async function loadFabric() {
     modloaderVersionCombo.css('display', "")
     modloaderVersionLabel.html('Fabric Loader');
+    let response = await $.get("/api/fabric/versions")
+    let items = "";
+    Array.from(response).forEach(item => {
+        items += `<div class="dropdown-item" value="${item.version}">${item.version}</div>`
+    })
+    modloaderVersionDropdown.find('.dropdown-items').html(items)
+    modloaderVersionDropdown.find('.dropdown-item').each((_, item) => {
+        target = $(item);
+        target.on('click', e => {
+            let target = $(e.currentTarget);
+            let dropdown = target.parent().parent()
+
+            dropdown.each((_, d) => {
+                d = $(d)
+                d.find('.dropdown-item.selected').removeClass('selected')
+            })
+
+            dropdown.find('.value').html(target.html())
+            dropdown.attr('value', target.attr('value'))
+            dropdown.blur()
+            target.addClass('selected');
+        })
+    })
+    modloaderVersionDropdown.find('.dropdown-items')[0].click();
 }
 async function loadForge() {
+    modloaderVersionDropdown.find('.dropdown-items').html("")
     modloaderVersionCombo.css('display', "")
     modloaderVersionLabel.html('Forge Loader');
 }
