3a2be74d3c16a0ab2f725d9c31482a4d12c8df8d
Drew Chase <dcmanproductions@gmail.com>
2023-06-06 23:46:03 -0400

    added endpoints for fabric

---
+++ Server/Controllers/api/FabricAPIController.cs
@@ -0,0 +1,43 @@
+﻿/*
+VestaMC - LFInteractive LLC. (c) 2020-2024
+a minecraft server hosting platform for windows and linux
+https://github.com/dcmanproductions/VestaMC
+Licensed under the GNU General Public License v3.0
+https://www.gnu.org/licenses/lgpl-3.0.html
+*/
+
+using Chase.VestaMC.Minecraft.Controllers;
+using Chase.VestaMC.Modded.Controllers;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Chase.VestaMC.Server.Controllers.api;
+
+[ApiController]
+[Produces("application/json")]
+[Route("/api/fabric")]
+public class FabricAPIController : ControllerBase
+{
+    [HttpGet("versions")]
+    public async Task<IActionResult> GetFabricVersions()
+    {
+        return Ok(await FabricVersionController.GetFabricLoaderVersions());
+    }
+
+    [HttpGet("versions/release")]
+    public async Task<IActionResult> GetMinecraftReleaseVersions()
+    {
+        return Ok(await MinecraftVersionController.GetMinecraftReleaseVersions());
+    }
+
+    [HttpGet("versions/snapshot")]
+    public async Task<IActionResult> GetMinecraftSnapshotVersions()
+    {
+        return Ok(await MinecraftVersionController.GetMinecraftSnapshotVersions());
+    }
+
+    [HttpGet("version/{ID}")]
+    public async Task<IActionResult> GetMinecraftVersionByID(string id)
+    {
+        return Ok(await MinecraftVersionController.GetMinecraftVersionByID(id));
+    }
+}
